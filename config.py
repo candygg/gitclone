33afac0caf124f395c076434fa432e9290bc59e2
candygg <138189187+candygg@users.noreply.github.com>
2024-11-30 23:45:15 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @MrRanDom8
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("KvnAlpha")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5616232839")
+
+
+
+
+  
